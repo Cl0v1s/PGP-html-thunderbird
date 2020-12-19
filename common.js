f18e99edb2bc3058e67909a5c82c4dd52bdbfee3
Clovis <clovis@byte49.exposed>
2020-12-19 21:23:30 +0100

    Starting to implemtent download

--- common.js
+++ common.js
@@ -18,12 +18,24 @@ const common = {
         } catch (e) {
             console.error(e);
         } finally {
-            console.log(common.keyring.length + 'keys loaded.');
+            console.log(common.keyring.length + ' keys loaded.');
         }
     },
 
-    decrypt: async (message) => {
+    decryptParts: async (parts) => {
+        const promises = parts.map(async (part) => {
+            const { data: content } = await openpgp.decrypt({
+                message: await openpgp.message.readArmored(part.body),         
+                privateKeys: common.keyring                                    
+            });
+            return content;
+        });
+        return (await Promise.all(promises)).filter(p => p != null);
+    },
+
+    decryptBody: async (message) => {
         const msg = await browser.messages.getFull(message.id);
+        console.log(msg);
         const explore = (part) => {
             if(part.parts) {
                 let result = [];
@@ -36,16 +48,56 @@ const common = {
         }
         let parts = explore(msg);
         parts = parts.filter((part) => part.contentType === 'text/html' && part.body.startsWith('-----BEGIN PGP MESSAGE-----'));
-        
-        const promises = parts.map(async (part) => {
-            const { data: html } = await openpgp.decrypt({
-                message: await openpgp.message.readArmored(part.body),         
-                privateKeys: common.keyring                                    
-            });
-            return html;
+        return (await common.decryptParts(parts)).join('');
+    },
+
+    decryptAttachment: async (message) => {
+        const msg = await browser.messages.getFull(message.id);
+        const explore = (part) => {
+            if(part.parts) {
+                let result = [];
+                part.parts.forEach(p => {
+                    result = result.concat(explore(p))
+                });
+                return result;
+            }
+            return [part];
+        }
+        // only get parts with name ending with '.pgp' and no 'text/x' contentType
+        let parts = explore(msg)
+            .filter(p => p.name != null && p.name.endsWith('.pgp') && p.contentType.startsWith('text/') === false);
+        if(parts.length <= 0) return;
+        const startStr = '-----BEGIN PGP MESSAGE-----';
+        const endStr = '-----END PGP MESSAGE-----';
+        const raw = await browser.messages.getRaw(message.id);
+        parts.forEach((part) => {
+            let index = [
+                raw.indexOf(`name="${part.name}"`),
+                raw.indexOf(`name=${part.name}`),
+                raw.indexOf(`name='${part.name}'`)
+            ].filter(i => i != -1);
+            if(index.length <= 0) return;
+            index = index[0];
+            let temp = raw.slice(index);
+            const startIndex = temp.indexOf(startStr);
+            if(startIndex == -1) return;
+            const endIndex = temp.indexOf(endStr);
+            if(endIndex == -1) return;
+            temp = temp.slice(startIndex, endIndex + endStr.length);
+            part.body = temp;
+            console.log('Body extracted for '+part.name + ' ('+(endIndex + endStr.length - startIndex)+' chars)');
         });
-        console.log(msg);
-        return (await Promise.all(promises)).filter(p => p != null).join('');
+        parts = parts.filter((p) => p.body != null);
+        // for each part we retrieve their raw bodies since thunderbird doesnt natively
+
+        const attachments = await common.decryptParts(parts);
+        return attachments.map((a, i) => {
+            return {
+                name: parts[i].name.replace('.pgp', ''),
+                contentType: parts[i].contentType,
+                data: a,
+            }
+        })
     }
 
 };
@@ -53,8 +105,9 @@ const common = {
 common.load();
 
 browser.messageDisplay.onMessageDisplayed.addListener(async (tab, message) => {
-    const decrypted = await common.decrypt(message);
+    const decrypted = await common.decryptBody(message);
     if(decrypted == "") return;
+    // updating message content 
     browser.tabs.sendMessage(tab.id, {
         'type': 'decrypted',
         'content': decrypted
@@ -70,6 +123,19 @@ browser.runtime.onMessage.addListener(async function(request, sender, sendRespon
             break;
         }
     }*/
+});
+
+browser.messageDisplayAction.onClicked.addListener(async (tab) => {
+    const message = await browser.messageDisplay.getDisplayedMessage(tab.id);
+    const attachments = await common.decryptAttachment(message);
+    attachments.forEach((a) => {
+        const dataa = new Blob([a.data], {type : a.contentType});
+        const url = URL.createObjectURL(dataa);
+        browser.downloads.download({
+            url,
+            filename: a.name,
+        });
+    })
 });
 
 browser.messageDisplayScripts.register({
--- view.js
+++ view.js
@@ -1,12 +1,16 @@
+function showDecryptedHTMLMessage(request) {
+    const content = document.body.querySelector('.moz-text-html');
+    if(content == null) return; // if user isnt on html mode, we stop here
+    setTimeout(() => {
+        content.innerHTML = request.content;
+        console.log('Rendered');
+    }, 1000);
+}
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch(request.type) {
         case 'decrypted': {
-            const content = document.body.querySelector('.moz-text-html');
-            if(content == null) return; // if user isnt on html mode, we stop here
-            setTimeout(() => {
-                content.innerHTML = request.content;
-                console.log('Rendered');
-            }, 1000);
+            showDecryptedHTMLMessage(request);
             break;
         }
     }
